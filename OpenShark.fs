0600af34d69c120d9026d4fba00a315011068ee5
Heather Cynede <heather@cynede.net>
2013-04-12 09:36:20 +0400

    #if __MonoCS__ -> weird style icon load

--- OpenShark.fs
+++ OpenShark.fs
@@ -110,29 +110,30 @@ type KeyboardHook() as X =
 #endif
 /// __________________________________________________________________________________________________________
 open System.Reflection
-type IconResource (name : string, res : bool) =
-    let loadIconResource name res =
-        if res then
-            let assm = Assembly.GetExecutingAssembly()
-            let res = assm.GetManifestResourceStream(name)
-            new Icon(res)
-        else 
-            let pths = [
-                "../../";
-                "../"
-                ]
-            let rec tryLoad(x) =
-                let p = 
-                    if x = -1 then  name                        
-                    else pths.[x] + name
-                if File.Exists p then
-                    new Icon(p : string)
-                else 
-                    if x < pths.Length  then tryLoad(x + 1)
-                    else null
-            tryLoad -1
+type IconResource (name : string) =
+    let loadIconResource name =
+        #if __MonoCS__
+        let pths = [
+            "../../";
+            "../"
+            ]
+        let rec tryLoad(x) =
+            let p = 
+                if x = -1 then  name                        
+                else pths.[x] + name
+            if File.Exists p then
+                new Icon(p : string)
+            else 
+                if x < pths.Length  then tryLoad(x + 1)
+                else null
+        tryLoad -1
+        #else
+        let assm = Assembly.GetExecutingAssembly()
+        let res = assm.GetManifestResourceStream(name)
+        new Icon(res)
+        #endif
 
-    let image = loadIconResource name res
+    let image = loadIconResource name
 
     member __.Name = name
     member __.Icon = image
@@ -140,7 +141,7 @@ type IconResource (name : string, res : bool) =
 [<RequireQualifiedAccess>]
 [<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>] 
 module IconResource =
-    let OpenShark = IconResource ("OpenShark.ico", true)
+    let OpenShark = IconResource "OpenShark.ico"
 /// __________________________________________________________________________________________________________
 #if __MonoCS__
     // TODO: conf file
